84a44fd58f46686f36772e2221538631ed272138
somyaranjan <somyaranjankhatua0@gmail.com>
2023-04-28 13:54:04 +0530

    adding interface for dinance pool

--- smart_contract/contracts/DinanceFactory.sol
+++ smart_contract/contracts/DinanceFactory.sol
@@ -1,6 +1,22 @@
 /* SPDX-License-Identifier: MIT */
 pragma solidity ^0.8.7;
 
+import "./DinancePool.sol";
+import "./IDinancePool.sol";
+
 contract DinanceFactory {
+
+    event poolCreated(address token, address pool, uint poolLength);
     
+    function createPool(address token) public returns(address pool) {
+        bytes memory bytecode = type(DinancePool).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token));
+
+        assembly {
+            pool := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+    }
+
+
+
 }
