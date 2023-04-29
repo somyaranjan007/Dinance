ade13dfa9b6d46b4d5a7a9a9c71d6c114a97b100
somyaranjan <somyaranjankhatua0@gmail.com>
2023-04-29 14:05:02 +0530

    adding pools

--- smart_contract/contracts/DinancePool.sol
+++ smart_contract/contracts/DinancePool.sol
@@ -2,16 +2,40 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./DinanceFactory.sol";
+import "./DinanceAToken.sol";
+
+error PoolDoesntExist(string message);
 
 contract DinancePool {
-    address public factory;
     address public token;
-    mapping(address=>uint256) depositor;
-    mapping(adresss=>uint256) depositedTime;
-    
-    uint256 constant interest=(2 * 1e9) /(365 * 24 * 60 * 60);
-    constructor() {
+    address public factory;
+    mapping(address => uint256) depositor;
+    mapping(adresss => uint256) depositedTime;
+    mapping(address => DinanceAToken) AToken;
+
+    struct TokenProperty {
+        address tokenAddress;
+        string name;
+        string symbol;
+    }
+
+    DinanceFactory pool = new DinanceFactory();
+
+    uint256 constant interest = (2 * 1e9) / (365 * 24 * 60 * 60);
+    uint256 constant borrowInterest = (2 * 1e9) / (365 * 24 * 60 * 60);
+
+    constructor(TokenProperty[] memory _tokenPools) {
         factory = msg.sender;
+        for (uint256 i = 0; i < _tokenPools.length; i++) {
+            address createdPool = pool.createPool(_tokenPools[i].tokenAddress);
+
+            AToken[_tokenPools[i].tokenAddress] = new DinanceAToken(
+                _tokenPools[i].tokenAddress,
+                _tokenPools[i].name,
+                _tokenPools[i].symbol
+            );
+        }
     }
 
     function initialize(address _token) external {
@@ -19,43 +43,37 @@ contract DinancePool {
         token = _token;
     }
 
-    function createAToken(
-        address _token,
-        string memory _name,
-        string memory _symbol
-    ) internal {
-        AToken[_token] = new DinanceAToken(_token, _name, _symbol);
-    }
-
     function deposit(
         address _token,
-        uint256 _amount,
-        address onBehalfOf,
-        string memory _name,
-        string memory _symbol
+        uint256 _amount
     ) external {
-        depositor[msg.sender]+=amount;
-        depositedTime[msg.sender]=block.timestamp;
-        createAToken(_token, _name, _symbol);
+        bool poolExist = pool.checkPool(_token);
+        if (!poolExist) {
+            revert PoolDoesntExist("You are depositing wrong token!");
+        }
+
+        depositor[msg.sender] += amount;
+        depositedTime[msg.sender] = block.timestamp;
         IERC20(_token).transferFrom(msg.sender, address(this), _amount);
         IERC20(AToken[_token]).mint(msg.sender, _amount);
     }
 
-    function withdraw(address _token,uint256 _amount,address to) external{
-        uint256 amountAfterInterest=((depositedTime[msg.sender]-block.timestamp) * interest) + _amount;  
-        IERC20(AToken[_token].burn(to,_amount));
-        IERC20(_token).transfer(to,_amount );
-
+    function withdraw(address _token, uint256 _amount, address to) external {
+        uint256 amountAfterInterest = ((depositedTime[msg.sender] -
+            block.timestamp) * interest) + _amount;
+        IERC20(AToken[_token].burn(to, _amount));
+        IERC20(_token).transfer(to, _amount);
     }
 
-    function borrow(address _token,uint256 _amount,address onBehalfOf) external{
-        //user will borrow from the deposited token
-        //user will charge with the interest 
-        
-        
-    }
+    function borrow(
+        address _token,
+        uint256 _amount,
+        address onBehalfOf
+    ) external {}
 
-    function repay(address _token,uint256 _amount,address onBehalfOf) external {
-
-    }
+    function repay(
+        address _token,
+        uint256 _amount,
+        address onBehalfOf
+    ) external {}
 }
