6dab9af75aac5589fb523488b6c64476da484ec3
darab ahmad khan <aksam25@gmail.com>
2023-04-29 14:06:10 +0530

    updating

--- smart_contract/contracts/DinancePool.sol
+++ smart_contract/contracts/DinancePool.sol
@@ -7,9 +7,12 @@ contract DinancePool {
     address public factory;
     address public token;
     mapping(address=>uint256) depositor;
-    mapping(adresss=>uint256) depositedTime;
+    mapping(address=>uint256) depositedTime;
+    mapping(address=>uint256) borrower;
+    mapping(address=>uint256) borrowedTime;
     
-    uint256 constant interest=(2 * 1e9) /(365 * 24 * 60 * 60);
+    uint256 public constant interest=(2 * 1e9) /(365 * 24 * 60 * 60);
+    uint256 public constant borrowInterest=(4 * 1e9) /(365 * 24 * 60 * 60);
     constructor() {
         factory = msg.sender;
     }
@@ -42,20 +45,28 @@ contract DinancePool {
     }
 
     function withdraw(address _token,uint256 _amount,address to) external{
+        require(depositor[msg.sender]>=_amount,"dont have any amount to withdraw");
         uint256 amountAfterInterest=((depositedTime[msg.sender]-block.timestamp) * interest) + _amount;  
         IERC20(AToken[_token].burn(to,_amount));
-        IERC20(_token).transfer(to,_amount );
+        depositor[msg.sender]=0;
+        depositedTime[msg.sender]=0;
+        IERC20(_token).transfer(to,amountAfterInterest);
 
     }
 
     function borrow(address _token,uint256 _amount,address onBehalfOf) external{
+        borrower[msg.sender]+=_amount;
+        borrowedTime[msg.sender]=block.timestamp;
         //user will borrow from the deposited token
+        IERC20(_token).transfer(msg.sender,_amount);
         //user will charge with the interest 
-        
-        
+ 
     }
 
     function repay(address _token,uint256 _amount,address onBehalfOf) external {
+        require(borrower[msg.sender]>=_amount,"you havent borrowed anytoken yet");
+        uint256 amountAfterInterestCharged=_amount - ((borrowedTime[msg.sender]-block.timestamp) * borrowInterest);
+         
 
     }
 }
