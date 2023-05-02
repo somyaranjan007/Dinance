6fae5c386e6fdf5d15223b83d758e53e22dd4317
darab ahmad khan <aksam25@gmail.com>
2023-05-02 15:58:07 +0530

    updating..

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -4,26 +4,9 @@ import { ethers } from "ethers";
 import detectEthereumProvider from "@metamask/detect-provider";
 
 const Header = () => {
-<<<<<<< HEAD
   const connectWallet = async () => {
     try {
       const provider = await detectEthereumProvider();
-=======
-    return (
-        <div className="flex bg-[#1f212d] text-white  items-center justify-between px-2 h-[70px] pr-4">
-            <div className="flex items-center">
-                <img src={Logo.src} alt="" className="w-[50px]" />
-                <h1 className="text-[35px]">Dinance</h1>
-            </div>
-            <div className="flex items-center">
-                <ul className="flex items-center h-full mx-5">
-                    <li className="px-3 text-[16px]">Dashboard</li>
-                    <li className="px-3 text-[16px]">Markets</li>
-                    <li className="px-3 text-[16px]">Stake</li>
-                    <li className="px-3 text-[16px]">Governance</li>
-                    <li className="px-3 text-[16px]">More</li>
-                </ul>
->>>>>>> global_supply
 
       if (provider) {
         const account = await provider.request({
