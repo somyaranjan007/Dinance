f3f430cb3f5270d710f9b879d5f13851d45b86bc
darab ahmad khan <aksam25@gmail.com>
2023-05-02 22:21:06 +0530

    DinanceDeploy and DinanaceTest updated

--- smart_contract/test/DinanceTest.js
+++ smart_contract/test/DinanceTest.js
@@ -7,22 +7,22 @@ describe("Dinance: The Lending and Borrowing Protocol Testing", function() {
 
     const poolToken = [
         {
-            tokenAddress: 0x6B175474E89094C44Da98b954EedeAC495271d0F,
+            tokenAddress: "0x68194a729C2450ad26072b3D33ADaCbcef39D574",
             name: "aDAIToken",
             symbol: "aDAI"
         },
         {
-            tokenAddress: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
+            tokenAddress: "0xda9d4f9b69ac6C22e444eD9aF0CfC043b7a7f53f",
             name: "aUSDCToken",
             symbol: "aUSDC"
         },
         {
-            tokenAddress: 0xdAC17F958D2ee523a2206206994597C13D831ec7,
+            tokenAddress: "0x0Bd5F04B456ab34a2aB3e9d556Fe5b3A41A0BC8D",
             name: "aUSDTToken",     
             symbol: "aUSDT"
         },
         {
-            tokenAddress: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
+            tokenAddress: "0xD0dF82dE051244f04BfF3A8bB1f62E1cD39eED92",
             name: "aWETHToken",
             symbol: "aWETH"
         }
