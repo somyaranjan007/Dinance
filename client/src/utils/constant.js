7f1c33d9b5f4df8ac5135629c1163aecb004c64b
somyaranjan007 <somyaranjankhatua122@gmail.com>
2025-02-04 02:06:20 +0530

    contract updated

--- client/src/utils/constant.js
+++
@@ -1,7 +0,0 @@
-import factoryABI from "./DinanceFactory.json";
-import poolABI from "./DinancePool.json";
-
-export const DinanceFactoryAddress = "0xa76a02958fc0918051D92658Ee1AfCb74944724B";
-export const DinanceFactoryABI = factoryABI.abi;
-
-export const DinancePoolABI=poolABI.abi
